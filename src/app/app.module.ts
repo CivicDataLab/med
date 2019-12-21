22dae239ca5249edb82d19f44d3df81bfe2415e2
DC <eopoxf@gmail.com>
2019-12-22 01:46:42 +0530

    Add topbar to application home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TopBarComponent } from './top-bar/top-bar.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
