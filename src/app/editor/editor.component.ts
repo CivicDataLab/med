192cf62b8ff431c39990049261a4d17dfc3b45fb
DC <eopoxf@gmail.com>
2019-12-22 18:11:52 +0530

    Add auto transliterate

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -6,52 +6,58 @@ import { ConfigService } from '../config.service';
 import { Transliteration } from './Transliteration';
 
 @Component({
-  selector: 'app-editor',
-  templateUrl: './editor.component.html',
-  styleUrls: ['./editor.component.scss']
+    selector: 'app-editor',
+    templateUrl: './editor.component.html',
+    styleUrls: ['./editor.component.scss']
 })
 export class EditorComponent implements OnInit {
-  form: FormGroup;
-  @ViewChild('editor', {
-    static: true
-  }) editor: QuillEditorComponent;
+    form: FormGroup;
+    @ViewChild('srcEditor', {
+        static: true
+    }) srcEditor: QuillEditorComponent;
 
-  constructor(fb: FormBuilder, private configService: ConfigService) {
-    this.form = fb.group({
-      editor: ['']
-    });
-  }
+    constructor(fb: FormBuilder, private configService: ConfigService) {
+        this.form = fb.group({
+        srcEditor: [''],
+        tgtEditor: ['']
+        });
+    }
 
-  ngOnInit() {
-    this.form
-      .controls
-      .editor
-      .valueChanges.pipe(
-        debounceTime(400),
-        distinctUntilChanged()
-      )
-      .subscribe((data) => {
-        // tslint:disable-next-line:no-console
-        console.log('native fromControl value changes with debounce', data);
-      });
-  }
+    ngOnInit() {
+        this.form
+        .controls
+        .srcEditor
+        .valueChanges.pipe(
+            debounceTime(400),
+            distinctUntilChanged()
+        )
+        .subscribe((data) => {
+            // tslint:disable-next-line:no-console
+            console.log('native fromControl value changes with debounce', data);
+            this.configService.getTransliteration(`${this.form.get('srcEditor').value}`)
+            .subscribe( (res: Transliteration) => {
+                this.form.get('tgtEditor').patchValue(res.transliteration[0]);
+            });
 
-  setControl() {
-    this.form.setControl('editor', new FormControl('test - new Control'));
-  }
+        });
+    }
 
-  patchValue() {
-    this.form.get('editor').patchValue(`${this.form.get('editor').value} patched!`);
-  }
+    setControl() {
+        this.form.setControl('srcEditor', new FormControl('test - new Control'));
+    }
 
-  transliterate() {
-      console.log(`${this.form.get('editor').value}`);
-      console.log(`${this.form.controls.editor.value}`);
-      this.configService.getTransliteration(`${this.form.get('editor').value}`)
-      .subscribe( (res: Transliteration) => {
-              this.form.get('editor').patchValue(res.transliteration[0]);
-          });
-      // this.form.get('editor').patchValue(`Transliterated content`);
-  }
+    patchValue() {
+        this.form.get('srcEditor').patchValue(`${this.form.get('editor').value} patched!`);
+    }
+
+    transliterate() {
+        console.log(`${this.form.get('srcEditor').value}`);
+        console.log(`${this.form.controls.srcEditor.value}`);
+        this.configService.getTransliteration(`${this.form.get('srcEditor').value}`)
+        .subscribe( (res: Transliteration) => {
+            this.form.get('srcEditor').patchValue(res.transliteration[0]);
+        });
+        // this.form.get('editor').patchValue(`Transliterated content`);
+    }
 
 }
