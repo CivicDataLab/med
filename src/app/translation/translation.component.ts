447cc44718cec813f50b63269dece1cf79fcf72d
DC <eopoxf@gmail.com>
2019-12-22 23:38:46 +0530

    Add basic setup for translation

--- src/app/translation/translation.component.ts
+++ src/app/translation/translation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'app-translation',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./translation.component.scss']
 })
 export class TranslationComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(fb: FormBuilder) { 
+    this.form = fb.group({
+      srcInput: '',
+      tgtInput: ''
+      });
+  }
 
   ngOnInit() {
+    this.form
+        .controls
+        .srcInput
+        .valueChanges.pipe(
+            debounceTime(400),
+            distinctUntilChanged()
+        )
+        .subscribe((data) => {
+            // tslint:disable-next-line:no-console
+            console.log('native fromControl value changes with debounce', data);
+
+        });  
   }
 
 }
