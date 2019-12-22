bbf9fd4bd15d46435ab87d4264c09f798a118342
DC <eopoxf@gmail.com>
2019-12-22 23:49:08 +0530

    add translation service definition

--- src/app/Services/translation.service.ts
+++ src/app/Services/translation.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Translation } from '../translation/Translation';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TranslationService {
 
+export class TranslationService {
+  
   constructor(private http: HttpClient) { }
+
+  base_url = 'http://localhost:5000';
+
+  getTranslation ( text: string ) {
+    return this
+                .http
+                .get<Translation>(`${this.base_url}/lookup?word=${text}`);
+  }
 }
